67399a5fccff5c927c29fdd21fd83cde375471b1
K23HP <99390996+K23HP@users.noreply.github.com>
2023-06-27 16:51:57 +0800

    Update main.py

--- SimpleNeuralNetwork/main.py
+++ SimpleNeuralNetwork/main.py
@@ -4,10 +4,15 @@ from training.model_training import train_model
 
 def main():
     # Start training the model
-    train_model(epochs=20, batch_size=64, save=False)
+    train_model(
+        epochs=20, 
+        batch_size=64, 
+        save=True, 
+        model_name="my_new_model.pth"
+    )
     
     # Evaluate on the trained model
-    evaluate_model()
+    evaluate_model("model.pth")
 
 if __name__ == "__main__":
     main()
