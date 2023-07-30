f1ef35465275e81bbb4794f5f0afce38c33f9fc2
K23HP <99390996+K23HP@users.noreply.github.com>
2023-07-30 23:30:16 +0800

    Make minor changes

--- SimpleNeuralNetwork/scripts/training_devices.py
+++ SimpleNeuralNetwork/scripts/training_devices.py
@@ -3,10 +3,7 @@ import torch
 
 # Create a function to get cpu, gpu or mps device for training
 def get_training_device():
-    return (
-        "cuda"
-        if torch.cuda.is_available()
-        else "mps"
-        if torch.backends.mps.is_available()
-        else "cpu"
-    )
+    if torch.cuda.is_available():
+        return "cuda"
+    else:
+        return "mps" if torch.backends.mps.is_available() else "cpu"
